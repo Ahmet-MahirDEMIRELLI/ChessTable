9f65292dc867ac3691390f630e2c69c6a3cd1bd0
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-12-17 23:27:08 +0300

    GetPossibleMoves updated for Rook

--- Repositories/RookRepository.cs
+++ Repositories/RookRepository.cs
@@ -13,6 +13,169 @@ namespace ChessTable.Repositories
 
 		public List<Move> GetPossibleMoves(Board board, int row, int column, bool isWhite)
 		{
+			List<Move> possibleMoves = new List<Move>();
+			if (isWhite)
+			{
+				if (board.IsChecked)
+				{
+					if (board.BlacksCheckers[1].Row == -1) // çifte şah yok
+					{
+						Move eatingMove = GetEatingMoves(board, row, column, board.BlacksCheckers[0], isWhite);
+						if (eatingMove != null)
+						{
+							possibleMoves.Add(eatingMove);
+						}
+						List<Move> blockingMoves = GetBlockingMoves(board, row, column, board.BlacksCheckers[0], isWhite);
+						foreach (Move blockingMove in blockingMoves)
+						{
+							possibleMoves.Add(blockingMove);
+						}
+					}
+					return possibleMoves;
+				}
+			}
+			else
+			{
+				if (board.IsChecked)
+				{
+					if (board.WhitesCheckers[1].Row == -1) // çifte şah yok
+					{
+						Move eatingMove = GetEatingMoves(board, row, column, board.WhitesCheckers[0], isWhite);
+						if (eatingMove != null)
+						{
+							possibleMoves.Add(eatingMove);
+						}
+						List<Move> blockingMoves = GetBlockingMoves(board, row, column, board.WhitesCheckers[0], isWhite);
+						foreach (Move blockingMove in blockingMoves)
+						{
+							possibleMoves.Add(blockingMove);
+						}
+					}
+					return possibleMoves;
+				}
+			}
+
+			List<Move> normalMoves = GetNormalMoves(board, row, column, isWhite);
+			foreach (Move normalMove in normalMoves)
+			{
+				possibleMoves.Add(normalMove);
+			}
+
+			return possibleMoves;
+		}
+
+		private List<Move> GetBlockingMoves(Board board, int row, int column, Square checker, bool isWhite)
+		{
+			ThreadCheckRepository threadCheckRepository = new ThreadCheckRepository();
+			List<Move> possibleMoves = new List<Move>();
+			int type = threadCheckRepository.TraceToKing(board.BoardMatrix, checker.Row, checker.Col, isWhite ? board.WhiteKing.Row : board.BlackKing.Row, isWhite ? board.WhiteKing.Col : board.BlackKing.Col, isWhite);
+			List<Move> normalMoves = GetNormalMoves(board, row, column, isWhite);
+			switch (type)
+			{
+				case 1: // soldan şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						if (move.Row == checker.Row && move.Column > checker.Col)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 2: // sağdan şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						if (move.Row == checker.Row && move.Column < checker.Col)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 3: // üstten şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						if (move.Row > checker.Row && move.Column == checker.Col)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 4: // alttan şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						if (move.Row < checker.Row && move.Column == checker.Col)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 5: // sağ alttan şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						int rowDiff = move.Row - checker.Row;
+						int colDiff = move.Column - checker.Col;
+						if ((rowDiff == colDiff || rowDiff == -colDiff) && rowDiff < 0 && colDiff < 0)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 6: // sağ üstten şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						int rowDiff = move.Row - checker.Row;
+						int colDiff = move.Column - checker.Col;
+						if ((rowDiff == colDiff || rowDiff == -colDiff) && rowDiff > 0 && colDiff < 0)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 7: // sol üstten şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						int rowDiff = move.Row - checker.Row;
+						int colDiff = move.Column - checker.Col;
+						if ((rowDiff == colDiff || rowDiff == -colDiff) && rowDiff > 0 && colDiff > 0)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+				case 8: // sol alttan şah çekiliyor
+					foreach (Move move in normalMoves)
+					{
+						int rowDiff = move.Row - checker.Row;
+						int colDiff = move.Column - checker.Col;
+						if ((rowDiff == colDiff || rowDiff == -colDiff) && rowDiff < 0 && colDiff > 0)
+						{
+							possibleMoves.Add(move);
+						}
+					}
+					break;
+			}
+			return possibleMoves;
+		}
+
+		private Move GetEatingMoves(Board board, int row, int col, Square checker, bool isWhite)
+		{
+			int rowDiff = row - checker.Row;
+			int colDiff = col - checker.Col;
+			if (rowDiff == 0 || colDiff == 0) // şah çeken kalenin kolunda
+			{
+				var moves = GetNormalMoves(board, row, col, isWhite);
+				foreach (Move move in moves)
+				{
+					if (move.Row == checker.Row && move.Column == checker.Col)
+					{
+						return move;
+					}
+				}
+			}
+			return null;
+		}
+
+		private List<Move> GetNormalMoves(Board board, int row, int column, bool isWhite)
+		{
 			ThreadCheckRepository threadCheckRepository = new ThreadCheckRepository();
 			List<Move> possibleMoves = new List<Move>();
 			Move move;
@@ -23,7 +186,7 @@ namespace ChessTable.Repositories
 				if (isWhite)
 				{
 					var message = "";
-					if(row == 7 && column == 7 && !board.IsWhiteShortRookMoved) // kısa rok kalesini ilk hamlesi olucak
+					if (row == 7 && column == 7 && !board.IsWhiteShortRookMoved) // kısa rok kalesini ilk hamlesi olucak
 					{
 						message = "Whites Short Rooks First Move";
 					}
