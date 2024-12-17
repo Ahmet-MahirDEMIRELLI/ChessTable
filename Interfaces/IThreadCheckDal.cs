92bdc0f96c53858bf711f9800006d94dde27a8df
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-12-17 23:38:02 +0300

    commend line cleaning

--- Interfaces/IThreadCheckDal.cs
+++ Interfaces/IThreadCheckDal.cs
@@ -11,7 +11,6 @@ namespace ChessTable.Interfaces
 		Checker CheckQueen(byte[,] m, int row, int col, bool checkWhitePieces);
 		bool CheckKing(byte[,] m, int row, int col, bool checkWhitePieces);
 		bool CheckSquare(int row, int col);
-		//bool IsUnderCheck(Board board, bool checkWhiteKing);
 		bool IsMovable(byte[,] m, int pieceRow, int pieceColumn, int kingRow, int kingCol, bool isWhite);
 		int TraceToKing(byte[,] m, int pieceRow, int pieceColumn, int kingRow, int kingCol, bool isWhite);
 	}
